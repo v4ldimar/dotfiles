57923df529fb22504005eb89447a2beeb546ebec
valdimar <valdimar@retric.is>
2026-05-14 12:13:53 +0000

    add comment to config.sh

--- config.sh
+++ config.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
+# copy dotfiles to home directory
 
 DOTFILES=(.gitconfig .gitignore .zshrc)
-
 for dotfile in $(echo ${DOTFILES[*]});
 do
     cp ~/dotfiles/$(echo $dotfile) ~/$(echo $dotfile)
